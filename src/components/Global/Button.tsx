b5fcb7dcedacdf08c308e99509813d55eb12b610
Jadom <matquadev@gmail.com>
2023-05-28 21:49:03 -0300

    [add] Arrow in submit button

--- src/components/Global/Button.tsx
+++ src/components/Global/Button.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Button.scss";
+import { IoMdArrowForward } from "react-icons/io";
 
 export default function Button() {
   return (
     <button className="button" data-cursor-size={70} data-cursor-exclusion>
-      Contact Me
+      <span className="arrow">
+        <IoMdArrowForward />
+      </span>
+      Submit
     </button>
   );
 }
