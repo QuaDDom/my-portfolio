e83a143e4618340f5dff9e4639ba0383f9ca0db0
Jadom <matquadev@gmail.com>
2023-05-29 21:20:23 -0300

    [add, delete] ContactForm, Contact

--- src/components/Sections/Contact.tsx
+++ src/components/Sections/Contact.tsx
@@ -1,14 +1,47 @@
 import { Spacer } from "@nextui-org/react";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ScrollStaggerText from "../Text/ScrollStaggerText";
 import "./Contact.scss";
 import Line from "../Global/Line";
 import Button from "../Global/Button";
-import ContactForm from "../Contact/ContactForm";
 
 export default function Contact() {
+  const [isVisible, setIsVisible] = useState(false);
+  const componentRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry], observer) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.unobserve(entry.target); // Stop observing once it is visible
+        }
+      },
+      { threshold: 0.5 }
+    );
+
+    if (componentRef.current) {
+      observer.observe(componentRef.current);
+    }
+
+    return () => {
+      if (componentRef.current) {
+        observer.unobserve(componentRef.current);
+      }
+    };
+  }, []);
+
+  const handleClick = () => {
+    const email = "quaddomdev@gmail.com";
+    const subject = "Contact me";
+
+    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}`;
+
+    window.open(mailtoLink);
+  };
+
   return (
-    <div className="contact section" id="contact">
+    <div className="contact section" id="contact" ref={componentRef}>
       <ScrollStaggerText
         text={"Contact"}
         fontSize={100}
@@ -39,8 +72,20 @@ export default function Contact() {
         triggerAfter={1500}
       />
       <Spacer />
-      <ContactForm />s
-      <Button />
+      {isVisible && (
+        <div
+          className="emailButton"
+          data-cursor-size={90}
+          data-cursor-exclusion
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <p onClick={handleClick}>SEND ME AN EMAIL</p>
+        </div>
+      )}
+      {/* <Button /> */}
     </div>
   );
 }
